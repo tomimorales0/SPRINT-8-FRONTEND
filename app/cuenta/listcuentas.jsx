1ede3857efb19d209e7cb62af9aedb8cb96e8e56
Tomas Morales <tomasmorales004@gmail.com>
2024-12-12 20:12:11 -0300

    Agregue un display de numero de cuenta y de saldo en el apartado Cuenta.

--- app/cuenta/listcuentas.jsx
+++ app/cuenta/listcuentas.jsx
@@ -1,22 +1,32 @@
 'use client';
 
-export default function ClienteInfo({ cliente }) {
+export default function ClienteInfo({ cliente, saldo }) {
   return (
     <div className="grid gap-4">
-      <div className="p-4 bg-white shadow-md rounded-lg border border-gray-200">
-        <p className="text-lg text-gray-800">
+    
+      <div className="p-6 mt-4 rounded-lg">
+        <p className="text-5xl text-green-400 font-bold mt-2">
+          Saldo: ${saldo.saldo}
+        </p>
+        <p className="text-2xl text-black-600 font-bold">
+          Tu cuenta número: {saldo.numero_cuenta}
+        </p>
+      </div>
+
+      <div className="p-6 bg-white shadow-md rounded-lg border border-gray-200">
+        <p className="text-xl text-gray-800">
           <span className="font-medium">Nombre:</span> {cliente.nombre}
         </p>
-        <p className="text-lg text-gray-800">
+        <p className="text-xl text-gray-800">
           <span className="font-medium">Apellido:</span> {cliente.apellido}
         </p>
-        <p className="text-lg text-gray-800">
+        <p className="text-xl text-gray-800">
           <span className="font-medium">DNI:</span> {cliente.DNI}
         </p>
-        <p className="text-lg text-gray-800">
+        <p className="text-xl text-gray-800">
           <span className="font-medium">Tipo de cliente:</span> {cliente.tipo}
         </p>
-        <p className="text-lg text-gray-800">
+        <p className="text-xl text-gray-800">
           <span className="font-medium">Sucursal:</span> {cliente.sucursal}
         </p>
       </div>
