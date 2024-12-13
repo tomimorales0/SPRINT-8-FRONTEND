fc5978947255dd3ff68efd1d3a10e171be61a5b1
Tomas Morales <tomasmorales004@gmail.com>
2024-12-13 18:15:37 -0300

    Agregue el monto en transferencias y ademas corregi el display de la fecha

--- app/transferencias/[cuentas]/ListClient.jsx
+++ app/transferencias/[cuentas]/ListClient.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+function formatFecha(fecha) {
+  const opciones = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  const fechaFormateada = new Date(fecha).toLocaleDateString('es-ES', opciones);
+  return fechaFormateada;
+}
+
 export default function MovimientosList({ movimientos }) {
   return (
     <div>
@@ -9,7 +15,7 @@ export default function MovimientosList({ movimientos }) {
         <div className="grid gap-4">
           {movimientos.map((movimiento, index) => (
             <div
-              key={index} // Cambiado de `movimiento.id` porque no hay garantía de que todos tengan `id`
+              key={index}
               className="p-4 bg-white shadow-md rounded-lg border border-gray-200"
             >
               <p className="text-lg text-gray-800">
@@ -19,10 +25,13 @@ export default function MovimientosList({ movimientos }) {
                 <span className="font-medium">Hacia:</span> {movimiento.hacia}
               </p>
               <p className="text-lg text-gray-800">
+                <span className="font-medium">Monto:</span> ${movimiento.monto}
+              </p>
+              <p className="text-lg text-gray-800">
                 <span className="font-medium">Tipo:</span> {movimiento.tipo}
               </p>
               <p className="text-lg text-gray-800">
-                <span className="font-medium">Fecha:</span> {movimiento.fecha}
+                <span className="font-medium">Fecha:</span> {formatFecha(movimiento.fecha)}
               </p>
             </div>
           ))}
