db2e38baf73a9470acfb166c6038b60e75539cdf
Aromzio <aron231104@gmail.com>
2024-12-13 02:15:04 -0300

    commit servicios

--- app/transferencias/[cuentas]/formtranferencia.jsx
+++ app/transferencias/[cuentas]/formtranferencia.jsx
@@ -1,7 +1,7 @@
 "use server"
-export async function fetchMovimientos(clienteId) {
+export async function fetchMovimientos(cuentaId) {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/api/movimientos-por-cuenta?clienteId=${clienteId}`);
+    const response = await fetch(`http://127.0.0.1:8000/api/movimientos-por-cuenta?cuentaId=${cuentaId}`);
     if (!response.ok) {
       throw new Error("Error al obtener los movimientos");
     }
